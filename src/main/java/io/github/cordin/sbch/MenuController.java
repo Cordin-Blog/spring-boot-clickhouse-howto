0b12f295886b68a2c0686a8ece3e0db1852b981d
Cèsar Ordiñana <cordin@gmail.com>
2022-03-04 13:33:11 +0100

    Format and add headers

--- src/main/java/io/github/cordin/sbch/MenuController.java
+++ src/main/java/io/github/cordin/sbch/MenuController.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ *  Copyright 2022 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package io.github.cordin.sbch;
 
@@ -16,23 +28,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 class MenuController {
-	
-	private final MenuRepository menuRepository; 
 
-	/**
-	 * Creates a new instance.
-	 * @param menuRepository to get the menu data from
-	 */
-	public MenuController(MenuRepository menuRepository) {
-		this.menuRepository = menuRepository;
-	}
-	
-	/**
-	 * Provides the Averaged historical prices of dishes by decade.
-	 * @return the average dish price by decade as JSON
-	 */
-	@GetMapping("/avgdishprice")
-	public ResponseEntity<List<Map<String,Object>>> averagedDishPrice() {
-		return ResponseEntity.ok(menuRepository.averagedDishPrice());
-	}
+    private final MenuRepository menuRepository;
+
+    /**
+     * Creates a new instance.
+     * @param menuRepository to get the menu data from
+     */
+    public MenuController(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    /**
+     * Provides the Averaged historical prices of dishes by decade.
+     * @return the average dish price by decade as JSON
+     */
+    @GetMapping("/avgdishprice")
+    public ResponseEntity<List<Map<String, Object>>> averagedDishPrice() {
+        return ResponseEntity.ok(menuRepository.averagedDishPrice());
+    }
 }
--- src/main/java/io/github/cordin/sbch/MenuRepository.java
+++ src/main/java/io/github/cordin/sbch/MenuRepository.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ *  Copyright 2022 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package io.github.cordin.sbch;
 
@@ -21,35 +33,35 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MenuRepository {
-	
-	private static final String AVERAGED_DISH_PRICE_QUERY = """
-		SELECT
-		    round(toUInt32OrZero(extract(menu_date, '^\\d{4}')), -1) AS decade,
-		    count() AS dishes,
-		    round(avg(price), 2) as avgPrice
-		FROM menu_item_denorm
-		WHERE (menu_currency = 'Dollars') AND (decade > 0) AND (decade < 2022)
-		GROUP BY decade
-		ORDER BY decade ASC;
-    """;
 
-	private final JdbcTemplate template;
+    private static final String AVERAGED_DISH_PRICE_QUERY = """
+            SELECT
+                round(toUInt32OrZero(extract(menu_date, '^\\d{4}')), -1) AS decade,
+                count() AS dishes,
+                round(avg(price), 2) as avgPrice
+            FROM menu_item_denorm
+            WHERE (menu_currency = 'Dollars') AND (decade > 0) AND (decade < 2022)
+            GROUP BY decade
+            ORDER BY decade ASC;
+              """;
 
-	/**
-	 * Creates a new instance.
-	 * @param template to use to perform JDBC queries to the ClickHouse database.
-	 */
-	MenuRepository(JdbcTemplate template) {
-		this.template = template;
-	}
-	
-	/**
-	 * Provides the Averaged historical prices of dishes by decade.
-	 * 
-	 * @see https://clickhouse.com/docs/en/getting-started/example-datasets/menus/#query-averaged-historical-prices
-	 * @return the average dish price by decade
-	 */
-	public List<Map<String,Object>> averagedDishPrice() {
-		return template.queryForList(AVERAGED_DISH_PRICE_QUERY);
-	}
+    private final JdbcTemplate template;
+
+    /**
+     * Creates a new instance.
+     * @param template to use to perform JDBC queries to the ClickHouse database.
+     */
+    MenuRepository(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    /**
+     * Provides the Averaged historical prices of dishes by decade.
+     * 
+     * @see https://clickhouse.com/docs/en/getting-started/example-datasets/menus/#query-averaged-historical-prices
+     * @return the average dish price by decade
+     */
+    public List<Map<String, Object>> averagedDishPrice() {
+        return template.queryForList(AVERAGED_DISH_PRICE_QUERY);
+    }
 }
